9d7f9b201f956ba0acdfa2014a3d2e1e0d6a8d0f
govardanan-sl <govardanan.sl@rently.com>
2021-04-27 17:33:17 +0530

    register added

--- src/component/Login/Register.js
+++ src/component/Login/Register.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom';
 import loginImg from './login.svg';
 import './style.scss';
 
 const Register = () => {
+    const [username,setUsername] = useState('');
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        const profile = {name:username , email};
+        setIsPending(true);
+        let requestOptions = {
+            method: 'POST',
+            headers: {"Content-Type" : "application/json"},
+            body : JSON.stringify(profile)
+        };
+        let url = "https://backend-react-json-server.herokuapp.com/profile"
+          fetch(url, requestOptions)
+          .then((res) => {
+              if(res.status!==201){
+                throw Error(res.statusText);
+              }else{
+                console.log("New User Added");
+                setIsPending(false);
+                history.push('/react-blog-test-v2');
+              }
+          })
+          .catch((e) => {
+              console.log(e.message);
+              setIsPending(false);
+          });
+
+    }
     return (
         <div className="base-container">
         <div className="header"><h2>Register</h2></div>
@@ -10,27 +43,54 @@ const Register = () => {
         <div className="image">
             <img src={loginImg} alt="just a decoration"/>
         </div>
+        <form onSubmit={handleSubmit}>
         <div className="form">
-            <div className="form-group">
+             <div className="form-group">
                 <label htmlFor="username">Username</label>
-                <input type="text" name="username" placeholder="username" />
-            </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input type="password" name="password" placeholder="password" />
-        </div>
+                <input 
+                    type="text" 
+                    name="username" 
+                    placeholder="username" 
+                    value={username}
+                    onChange={(e)=>setUsername(e.target.value)}
+                    required
+                />
+             </div>
+             <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input 
+                    type="email" 
+                    name="email" 
+                    placeholder="email" 
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}
+                    required
+                />
+             </div>
+             <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input 
+                type="password" 
+                name="password" 
+                placeholder="password" 
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
+                required
+              />
+             </div>
         </div>
 
-        <button type="button" style = {{
+        {!isPending&&<button type="submit" style = {{
                     color:"white",
                     backgroundColor : "#f1356d",
                     borderRadius : '8px',
                     width: '7rem',
                     height: '3rem',
-                    marginLeft: '6rem'
+                    borderBlockColor:'pinks'
         }}>
-            Login
-        </button>
+            Register
+        </button>}
+        </form>
         </div>
         </div>
     );
